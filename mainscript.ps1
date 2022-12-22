4e8ebd241c14bc5b57996bd95f384581788fe28b
ImmortalX1 <88527501+ImmortalX1@users.noreply.github.com>
2022-12-22 20:27:35 +0530

    Create mainscript.ps1

---
+++ mainscript.ps1
@@ -0,0 +1,7 @@
+#install basic apps
+winget install --id=7zip.7zip -e  ; 
+winget install --id=VideoLAN.VLC -e  ; 
+winget install --id=Google.Chrome -e  ; 
+winget install --id=Spotify.Spotify -e  ; 
+winget install --id=DuongDieuPhap.ImageGlass -e  ; 
+winget install --id=Adobe.Acrobat.Reader.64-bit -e  ;
